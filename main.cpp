642f559d407ba8966ec5e46dc9ff51bb0fda5c77
pulver22 <pulver22@gmail.com>
2015-02-20 19:31:26 +0100

    Added the scatter snippet {I don't know if it works...i get segfault]

--- main.cpp
+++ main.cpp
@@ -12,8 +12,9 @@ using namespace std;
 
 int Read_File_PGM(string filename, int argc, char *argv[])
 {
-    int row = 0, col = 0, numrows = 0, numcols = 0;
-    int rank,size;
+    int row = 0, col = 0;
+    int rank;
+    int numTask = 0, sendCount, recvCount, source;
     std::stringstream ss;
     ifstream infile(filename.c_str(), ios::binary);
     ifstream infile2(filename.c_str(), ios::binary);
@@ -27,42 +28,25 @@ int Read_File_PGM(string filename, int argc, char *argv[])
     for (num_line = 0; getline(infile2, line); ++num_line)
         ;
 
-    //create a matrix containing the same number of line of the image
+    //create a matrix containing the same number of line of the image and a matrix to store the value send by other process
     string img[num_line];
+    string recvBuf[num_line / 4];
+    string partialResult[num_line/4];
+
+    for (int i=0; i < 4; ++i){
+    	getline(infile,inputLine);
+    	if (i == 1 && inputLine.at(0) == '#'){
+    		f << "#Gamma corrected by pulver";
+    	}
+    	if (i == 3 && inputLine.length() > 4){
+    		break;
+    	}
+    	f << inputLine << endl;
+    }
 
 
-    // First line : version
-    getline(infile,inputLine);
-    cout << "Version : " << inputLine << endl;
 
-    // Second line : comment + width and height
-
-    /*
-    getline(infile,comment);
-    cout << comment << endl;
-    */
     ss << infile.rdbuf();
-
-	ss >> numrows >> numcols;
-
-	// Third line: max bitsof grey scale
-    int max_bits;
-    ss >> max_bits;
-
-
-    cout << "rows: " << numrows << "; cols: " << numcols << endl;
-    cout << "max size: " << max_bits ;
-
-    f << "P2" << endl ;
-
-
-
-    /*
-    if (comment.at(0) == '#'){
-    	f << "# Gamma modified by pulver" << endl;
-    }*/
-    f << numrows << " " << numcols << endl;
-    f << "255" << endl;
 
     //Following lines: fill the img matrix
        for(row = 0; row < num_line; ++row){
@@ -72,12 +56,51 @@ int Read_File_PGM(string filename, int argc, char *argv[])
 
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_size(MPI_COMM_WORLD, &numTask);
+
+    if (numTask == 4 ){
+    	int source = 0;
+    	int sendCount = 4;
+    	int recvCount = 4;
+
+    	MPI_Scatter(img, sendCount,MPI_INT,recvBuf, recvCount,MPI_INT, source,MPI_COMM_WORLD);
+
+    	for (row = 0; row < num_line/4; ++row){
+    		int lenght = recvBuf[row].length();
+    		int num, temp = 0;
+    		char space = ' ';
+    		double gamma = 2.2;
+    		std::stringstream ss;
+    		for (col = 0; col < lenght; ++col){
 
 
+    			//until you read a number, put it in temp; when you read a space write temp in out and then the space
+    			if(recvBuf[row].at(col) != space){
+    				num = recvBuf[row].at(col) -'0' ;
+    				temp = temp * 10 + num;
+
+    			}else{
+    				if(temp == 0) {
+    					ss << space;
+    				}else {
+    					int corrected = (int)(255 * pow(((double)temp/(double)255), gamma));
+    					ss << corrected << ' ';
+    					temp = 0;
+    				}
 
 
+    			}
+    		}
+    		getline(ss,partialResult[row]);
+    		cout << "rank= " << rank << " : " <<  ss << endl ;
+    	}
 
+    }else  {
+    	printf("Error, you must specify 4 tasks\n");
+    }
+
+
+    /*
     // Modify the gamma
         for (row = 0; row < num_line; ++row){
         	int lenght = img[row].length();
@@ -114,7 +137,7 @@ int Read_File_PGM(string filename, int argc, char *argv[])
             	}
 
             	f << endl;
-            }
+            }*/
 
 
         /*
@@ -163,6 +186,7 @@ int Read_File_PGM(string filename, int argc, char *argv[])
 
 infile.close();
 MPI_Finalize();
+return 0;
 
 }
 
